5c46a0bdd56905e65fcb1e4b0c9ecd874cc4d97d
MohitAgarwal17 <singhalmohitraj2@gmail.com>
2025-05-31 14:21:48 +0530

    Sum of 2 numbers

---
+++ Sum.java
@@ -0,0 +1,15 @@
+import  java.util.*;
+class Sum
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner(System.in);
+        int a,b;
+        int sum;
+        System.out.println("Enter 2 Numbers");
+        a=sc.nextInt();
+        b=sc.nextInt();
+        sum=a+b;
+        System.out.println("The Sum is"+ sum);
+    }
+}
